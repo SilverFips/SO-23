2f91a4779c2f1d48c172af0a1f5dc37d6d6c2147
Filipe Pedroso <fc51958@alunos.fc.ul.pt>
2021-03-18 16:47:24 +0000

    Adicionado Metodos auxiliares do proxy
    Obs: 2 imports no Syncronization

--- src/proxy.c
+++ src/proxy.c
@@ -13,6 +13,15 @@
 * métodos auxiliares definidos em proxy.h.
 */
 int execute_proxy(int proxy_id, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
+    while(true){
+        
+        //le op cliente
+        //op->id !=-1 && data->terminate == 0
+        //processa-a e escreve para servidor
+        //else op-> id == -1 ignore
+        // data->terminate == 1 -> terminar programa-> return numero de ops 
+    }
+
 
     return 0;
 }
@@ -24,19 +33,36 @@ int execute_proxy(int proxy_id, struct communication_buffers* buffers, struct ma
 * tentar ler a operação, deve verificar se data->terminate tem valor 1.
 * Em caso afirmativo, retorna imediatamente da função.
 */
-void proxy_receive_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){}
+void proxy_receive_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
+    consume_begin(sems->cli_prx);
+    semaphore_mutex_lock(sems->cli_prx->mutex);
+    read_circular_buffer(buffers->cli_prx, data->buffers_size, op);
+    semaphore_mutex_unlock(sems->cli_prx->mutex);
+    consume_end(sems->cli_prx);
 
+}
+    
 
 /* Função que processa uma operação, alterando o seu campo proxy para o id
 * passado como argumento, alterando o estado da mesma para 'P' (proxied), e 
 * incrementando o contador de operações.
 */
-void proxy_process_operation(struct operation* op, int server_id, int* counter){}
-
+void proxy_process_operation(struct operation* op, int server_id, int* counter){
+    op->proxy = server_id;
+    op->status = 'P';
+    counter++;
+}
+    
 
 /* Função que escreve uma operação no buffer de memória partilhada entre
 * proxies e servidores, efetuando a necessária sincronização antes e
 * depois de escrever.
 */
-void proxy_forward_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){}
+void proxy_forward_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
+    produce_begin(sems->prx_srv);
+    semaphore_mutex_lock(sems->prx_srv->mutex);
+    write_rnd_access_buffer(buffers->prx_srv, data->buffers_size, op);
+    semaphore_mutex_unlock(sems->prx_srv->mutex);
+    produce_end(sems->prx_srv);
+}
 
--- src/synchronization.c
+++ src/synchronization.c
@@ -1,4 +1,5 @@
-
+#include <unistd.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <semaphore.h>
 #include <stdio.h>
