2853dcbbf5af9118a294aa1151bfb53318346718
Filipe <filipe.n.pedroso@gmail.com>
2021-03-22 16:34:39 +0000

    added proxy to main

--- src/proxy.c
+++ src/proxy.c
@@ -1,7 +1,8 @@
-
-#include <stdio.h>
 #include "memory.h"
+#include "proxy.h"
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /* Função principal de um Proxy. Deve executar um ciclo infinito onde em 
 * cada iteração do ciclo lê uma operação dos clientes e se a mesma tiver id 
@@ -13,9 +14,25 @@
 * métodos auxiliares definidos em proxy.h.
 */
 int execute_proxy(int proxy_id, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
-
-    return 0;
+    int* count = malloc(sizeof(int));
+    struct operation* op = malloc(sizeof(struct operation));
+    while(1){
+        proxy_receive_operation(op, buffers, data, sems);
+        if((op->id != -1) && (*data->terminate == 0)){
+            proxy_process_operation(op, proxy_id, count);
+            proxy_forward_operation(op, buffers, data, sems);
+        }
+        if(*(data->terminate) == 1){
+            free(op);
+            int counter = *count;
+            free(count);
+            return counter;
+        }
+    }
 }
+
+        
+
 
 
 /* Função que lê uma operação do buffer de memória partilhada entre
@@ -24,19 +41,41 @@ int execute_proxy(int proxy_id, struct communication_buffers* buffers, struct ma
 * tentar ler a operação, deve verificar se data->terminate tem valor 1.
 * Em caso afirmativo, retorna imediatamente da função.
 */
-void proxy_receive_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){}
+void proxy_receive_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
+    
+    consume_begin(sems->cli_prx);
+    if(*(data->terminate)== 1){
+        consume_end(sems->cli_prx);
+        return;
+    }
+    semaphore_mutex_lock(sems->cli_prx->mutex);
+    read_circular_buffer(buffers->cli_prx, data->buffers_size, op);
+    semaphore_mutex_unlock(sems->cli_prx->mutex);
+    consume_end(sems->cli_prx);
 
+}
+    
 
 /* Função que processa uma operação, alterando o seu campo proxy para o id
 * passado como argumento, alterando o estado da mesma para 'P' (proxied), e 
 * incrementando o contador de operações.
 */
-void proxy_process_operation(struct operation* op, int server_id, int* counter){}
-
+void proxy_process_operation(struct operation* op, int server_id, int* counter){
+    op->proxy = server_id;
+    op->status = 'P';
+    (*counter)++;
+}
+    
 
 /* Função que escreve uma operação no buffer de memória partilhada entre
 * proxies e servidores, efetuando a necessária sincronização antes e
 * depois de escrever.
 */
-void proxy_forward_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){}
+void proxy_forward_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
+    produce_begin(sems->prx_srv);
+    semaphore_mutex_lock(sems->prx_srv->mutex);
+    write_rnd_access_buffer(buffers->prx_srv, data->buffers_size, op);
+    semaphore_mutex_unlock(sems->prx_srv->mutex);
+    produce_end(sems->prx_srv);
+}
 
