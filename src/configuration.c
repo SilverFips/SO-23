5bfa5ca305883a9b788a43a897f68c6f81b9556d
Filipe Pedroso <fc51958@alunos.fc.ul.pt>
2021-04-30 17:23:06 +0100

    added trim function to fix bin/log_file name

--- src/configuration.c
+++ src/configuration.c
@@ -8,12 +8,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 #include "log.h"
+#include <ctype.h> // for isspace on trimwhitespace
+
+
+
+
+char *trimwhitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
+}
+
 /*
-* Reads from the file that receives the arguments into the main_data struct
-* Creates the logfile, statisticfile
+* Reads from the file that receives the program arguments and writes them into the main_data structure
+* Calls the functions to create the log and statistic files
 */
+
 int open_begin_file(char* name_file, struct main_data* data){
     FILE *file;
     
@@ -28,30 +55,29 @@ int open_begin_file(char* name_file, struct main_data* data){
     while(fgets(line, sizeof(line), file) != NULL){
         if(i == 0){
             data->max_ops = atoi(line);
-            printf("\nmax ops: %d\n", data->max_ops);
         } else if(i == 1){
             data->buffers_size = atoi(line);
-            printf("\nbuffer_size: %d\n", data->buffers_size);
         } else if(i == 2){
             data->n_clients = atoi(line);
-            printf("\nn_clients: %d \n", data->n_clients);
         } else if(i == 3){
             data->n_proxies = atoi(line);
         } else if(i == 4){
             data->n_servers = atoi(line);
         } else if(i == 5){
-            char buffer[50];
-            sprintf(buffer, "./bin/%s", line);
-            printf("Nome ficheiro log: %s\n", buffer);
-            file_log_begin(buffer);
-        } else if(i == 6){
-            char buffer[50];
-            sprintf(buffer, "./bin/%s", line);
-            printf("Nome ficheiro statistics: %s\n", buffer);
-            statistic_file = fopen(buffer, "a");
-            if(statistic_file == NULL){
-                perror("creating statistic_file");
-            }
+            char filename[256]; 
+            char *token = strtok(line, ".");
+            sprintf(filename, "./bin/%s\n", token);
+            char *trimmedFileName = trimwhitespace(filename);
+
+            file_log_begin(trimmedFileName);
+        //} else if(i == 6){
+            // char buffer[50];
+            // sprintf(buffer, "./bin/%s", line);
+            // printf("Nome ficheiro statistics: %s\n", buffer);
+            // statistic_file = fopen(buffer, "a");
+            // if(statistic_file == NULL){
+            //     perror("creating statistic_file");
+        //     }
         } else if( i == 7){
             data->alarm = atoi(line);
             break;
@@ -59,11 +85,14 @@ int open_begin_file(char* name_file, struct main_data* data){
         i++;
     }
     fclose(file);
-    printf("%d\n", i);
+    
 
-    printf("max %d, size %d, clients %d, proxies  %d, servers %d\n", data->max_ops, data->buffers_size, data->n_clients, data->n_proxies, data->n_servers); 
+   
     return 0;
 }
+
+
+
     
 
 
